d17d2ee5636e95800774aac261fa47371bfdb9bc
RobzHunt <53803409+RobzHunt@users.noreply.github.com>
2026-04-23 09:07:26 -0300

    Update BaseController.java

--- Havana-Web/src/main/java/org/alexdev/http/controllers/BaseController.java
+++ Havana-Web/src/main/java/org/alexdev/http/controllers/BaseController.java
@@ -11,113 +11,79 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BaseController implements Route {
-    private static Logger logger = LoggerFactory.getLogger(BaseController.class);
 
     @Override
     public void handleRoute(WebConnection webConnection) throws Exception {
-        /*if (!webConnection.request().headers().isEmpty()) {
-            for (String name: webConnection.request().headers().names()) {
-                for (String value: webConnection.request().headers().getAll(name)) {
-                    System.out.println("HEADER: " + name + " = " + value);
-                }
-            }
-            System.out.println();
-        }*/
-
-        if (webConnection.isRequestHandled()) {
-            if (GameConfiguration.getInstance().getBoolean("maintenance") && !webConnection.getRouteRequest().startsWith("/api")) {
-                if (!webConnection.getRouteRequest().startsWith("/maintenance") && !webConnection.getRouteRequest().startsWith("/" + Routes.HOUSEKEEPING_PATH)) {
-                    webConnection.redirect("/maintenance");
-                    return;
-                }
-            }
+        if (!webConnection.isRequestHandled()) {
+            return;
         }
 
-        if (!webConnection.getRouteRequest().startsWith("/api")) {
-            if (!webConnection.request().headers().isEmpty()) {
-                String host = webConnection.request().headers().get(HttpHeaderNames.HOST);
-
-                if (webConnection.request().headers().contains("X-Forwarded-Proto")) {
-                    String request = webConnection.request().headers().get("X-Forwarded-Proto");
-
-                    if (host != null && request.equalsIgnoreCase("http")) {
-                        String targetUrl = "https://" + host;
-                        String requestUri = webConnection.request().uri();
-
-                        if (!requestUri.startsWith("/")) {
-                            targetUrl += "/";
-                        }
-
-                        targetUrl += requestUri;
-
-                        webConnection.movedpermanently(targetUrl);
-                        return;
-                    }
-                }
-            }
+        if (shouldRedirectToHttps(webConnection)) {
+            redirectToHttps(webConnection);
+            return;
         }
 
-        if (webConnection.isRequestHandled()) {
-            /*if (!(webConnection.getRouteRequest().equals("/register")
-                    || webConnection.getRouteRequest().equals("/client")
-                    || webConnection.getRouteRequest().equals("/login_popup")
-                    || webConnection.getRouteRequest().startsWith("/clientlog")
-                    || webConnection.getRouteRequest().equals("/client")
-                    || webConnection.getRouteRequest().equals("/security_check")
-                    || webConnection.getRouteRequest().startsWith("/account/")
-                    || webConnection.getRouteRequest().startsWith("/api/")
-                    || webConnection.getRouteRequest().startsWith("/habblet/")
-                    || webConnection.getRouteRequest().startsWith("/groups/actions")
-                    || webConnection.getRouteRequest().startsWith("/myhabbo/"))) {
-                webConnection.session().set("lastBrowsedPage", webConnection.getRouteRequest());
-            }*/
-
-            if (webConnection.session().getBoolean("authenticated")) {
-                this.handleAuthenticatedRoute(webConnection);
-            } else {
-                SessionUtil.checkCookie(webConnection);
-            }
+        if (isUnderMaintenance(webConnection)) {
+            webConnection.redirect("/maintenance");
+            return;
         }
+
+        if (webConnection.session().getBoolean("authenticated")) {
+            handleAuthenticatedRoute(webConnection);
+        } else {
+            SessionUtil.checkCookie(webConnection);
+        }
+    }
+
+    // ---------------------------------------------------------
+
+    private boolean shouldRedirectToHttps(WebConnection webConnection) {
+        if (webConnection.getRouteRequest().startsWith("/api")) {
+            return false;
+        }
+
+        var headers = webConnection.request().headers();
+
+        if (headers.isEmpty() || !headers.contains("X-Forwarded-Proto")) {
+            return false;
+        }
+
+        var proto = headers.get("X-Forwarded-Proto");
+        var host  = headers.get(HttpHeaderNames.HOST);
+
+        return host != null && proto.equalsIgnoreCase("http");
+    }
+
+    private void redirectToHttps(WebConnection webConnection) {
+        var host       = webConnection.request().headers().get(HttpHeaderNames.HOST);
+        var requestUri = webConnection.request().uri();
+        var separator  = requestUri.startsWith("/") ? "" : "/";
+
+        webConnection.movedpermanently("https://" + host + separator + requestUri);
+    }
+
+    private boolean isUnderMaintenance(WebConnection webConnection) {
+        if (!GameConfiguration.getInstance().getBoolean("maintenance")) {
+            return false;
+        }
+
+        var route = webConnection.getRouteRequest();
+
+        return !route.startsWith("/api")
+            && !route.startsWith("/maintenance")
+            && !route.startsWith("/" + Routes.HOUSEKEEPING_PATH);
     }
 
     private void handleAuthenticatedRoute(WebConnection webConnection) {
         if (webConnection.getRouteRequest().equals("/client")) {
-            webConnection.session().set("lastRequest", String.valueOf(DateUtil.getCurrentTimeSeconds() + SessionUtil.REAUTHENTICATE_TIME));
+            var expiry = DateUtil.getCurrentTimeSeconds() + SessionUtil.REAUTHENTICATE_TIME;
+            webConnection.session().set("lastRequest", String.valueOf(expiry));
         }
 
-        if (webConnection.session().contains("lastRequest")) {
-            long lastRequest = webConnection.session().getLongOrElse("lastRequest", 0);
+        var lastRequest  = webConnection.session().getLongOrElse("lastRequest", 0);
+        var sessionValid = lastRequest > 0
+            && DateUtil.getCurrentTimeSeconds() <= lastRequest;
 
-            if (DateUtil.getCurrentTimeSeconds() > lastRequest) {
-                //if (webConnection.cookies().exists(SessionUtil.REMEMEBER_TOKEN_NAME)) {
-                webConnection.session().set("clientAuthenticate", true);
-                //}
-            }
-        } else {
-            webConnection.session().set("clientAuthenticate", false);
-
-        }
-
-        /*StringBuilder postRequest = new StringBuilder("(");
-        StringBuilder getRequest = new StringBuilder();
-
-        for (var entry : webConnection.post().getValues().entrySet()) {
-            postRequest.append(entry.getKey());
-            postRequest.append(" = ");
-            postRequest.append(entry.getValue());
-            postRequest.append(", ");
-        }
-
-        for (var entry : webConnection.get().getValues().entrySet()) {
-            getRequest.append(entry.getKey());
-            getRequest.append(" = ");
-            getRequest.append(entry.getValue());
-            getRequest.append(", ");
-        }
-
-        getRequest.append(")");
-        postRequest.append(")");
-
-        logger.info("Request: " + webConnection.getUriRequest() + " from " + webConnection.getIpAddress() + " with payload POST: " + postRequest + ", GET: " + getRequest);*/
+        webConnection.session().set("clientAuthenticate", !sessionValid);
     }
 }
