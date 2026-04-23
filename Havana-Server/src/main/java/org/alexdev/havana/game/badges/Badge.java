829e1553148560ec48193d34f54385836d56a3ee
RobzHunt <53803409+RobzHunt@users.noreply.github.com>
2026-04-23 09:11:20 -0300

    Update Badge.java

--- Havana-Server/src/main/java/org/alexdev/havana/game/badges/Badge.java
+++ Havana-Server/src/main/java/org/alexdev/havana/game/badges/Badge.java
@@ -1,7 +1,7 @@
 package org.alexdev.havana.game.badges;
 
 public class Badge {
-    private String badgeCode;
+    private final String badgeCode; // nunca muda após criação
     private boolean equipped;
     private int slotId;
 
@@ -11,23 +11,15 @@ public class Badge {
         this.slotId = slotId;
     }
 
-    public String getBadgeCode() {
-        return badgeCode;
-    }
+    public String getBadgeCode() { return badgeCode; }
+    public boolean isEquipped()  { return equipped;  }
+    public int getSlotId()       { return slotId;    }
 
-    public boolean isEquipped() {
-        return equipped;
-    }
+    public void setEquipped(boolean equipped) { this.equipped = equipped; }
+    public void setSlotId(int slotId)         { this.slotId = slotId;    }
 
-    public void setEquipped(boolean equipped) {
-        this.equipped = equipped;
-    }
-
-    public int getSlotId() {
-        return slotId;
-    }
-
-    public void setSlotId(int slotId) {
-        this.slotId = slotId;
+    @Override
+    public String toString() {
+        return "Badge{code='" + badgeCode + "', equipped=" + equipped + ", slot=" + slotId + "}";
     }
 }
